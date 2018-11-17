dc89660f77fb4e8ad144476357df4cb8eb512f40
Jagadish, K <k.jagdish@gmail.com>
2018-11-18 00:44:35 +0530

    using pm2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = port.env.NODE_ENV === 'production' ? 80 : 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
