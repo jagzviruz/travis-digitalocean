527d8320d70db45ea230415caf9df2fe5b0de3b4
Jagadish, K <k.jagdish@gmail.com>
2018-11-18 10:45:55 +0530

    code error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = port.env.NODE_ENV === 'production' ? 80 : 3000
+const port = process.env.NODE_ENV === 'production' ? 80 : 3000
 
 app.get('/', (req, res) => res.send('Hello World v1!'))
 
