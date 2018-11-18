43e49fb41f58a81be33d8e2e32be0d97de5fa515
Jagadish, K <k.jagdish@gmail.com>
2018-11-18 11:07:50 +0530

    new endpoint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.NODE_ENV === 'production' ? 80 : 3000
 
-app.get('/', (req, res) => res.send('Hello World v1!'))
-
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/version', (req, res) => res.send(process.env.VERSION))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
